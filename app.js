a1b1252191ac1cf6f8e3ff27f012b1ee335f03af
matt-winzer <matt.winzer@gmail.com>
2019-06-13 12:10:46 -0600

    added bootstrap, navbar

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-const express = require('express')
-const app     = express()
-const PORT    = process.env.PORT || 3000
+require('dotenv').config()
+const express     = require('express')
+const stripe      = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const bodyParser  = require('body-parser')
+const morgan      = require('morgan')
+const exphbs      = require('express-handlebars')
+const PORT        = process.env.PORT || 3000
+
+const app = express()
+
+// View engine setup
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// Middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan('dev'))
+
+// Set static folder
+app.use(express.static(`${__dirname}/public`))
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 
 
