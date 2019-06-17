f45340d60694a4c380d56b2de416d2ac5b30064f
matt-winzer <matt.winzer@gmail.com>
2019-06-17 11:41:34 -0600

    refactored purchase script to seperate js file

---
+++ public/scripts/purchase.js
@@ -0,0 +1,22 @@
+const stripe = Stripe('pk_test_QSxJFjYgQXTkPvIyB4Q01xxg00qZ37aRqD')
+const purchaseButton = document.querySelector('#purchase')
+purchaseButton.addEventListener('click', async (e) => {
+  e.preventDefault()
+  try {
+    // Initiate checkout session to get session id
+    const response = await fetch('http://localhost:3000/checkout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    const data = await response.json()
+    const sessionId = data.session.id
+    // Redirect to checkout
+    const result = await stripe.redirectToCheckout({ sessionId })
+    console.log('STRIPE CHECKOUT RESULT', result)
+  } catch (error) {
+    console.log('STRIPE ERROR', error)
+  }
+})
